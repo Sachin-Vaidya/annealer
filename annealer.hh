d095e311fbd2e1bc2d64844dfeffc48493bd47db
Sachin-Vaidya <79388001+Sachin-Vaidya@users.noreply.github.com>
2025-08-14 14:15:27 -0400

    Update annealer.hh 08_14_25

--- annealer.hh
+++ annealer.hh
@@ -17,17 +17,21 @@ struct event_t;
 class QUBO {
     private:
         int n;
-
+        
         bool OTF;
         event_t* event; // optional pointer to event
         ftype max_D;
-
-        // use float for params to save memory
-        vector<pair<int, ftype>> affectedby_flat; // list of js that are affected by flipping a certain bit
-        vector<int> offset; // offset of each row in the flat list
+    
+        std::vector<std::pair<int, ftype>> affectedby_flat; // list of js that are affected by flipping a certain bit
+        std::vector<int> offset; // offset of each row in the flat list
+    
     public:
         QUBO(event_t& event); // OTF mode.
         QUBO(qubo_t& Q);
         ftype evaluate(const solution_t& x) const;
         ftype evaluateDiff(const solution_t& x, int flip_idx) const;
+    
+        // 🔧 Add these two method declarations:
+        const std::vector<std::pair<int, ftype>>& getAffectedByFlat() const;
+        const std::vector<int>& getOffset() const;
 };
