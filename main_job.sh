cf29a1afe312ce421da7cda0e9650b3d2925863a
Sachin-Vaidya <79388001+Sachin-Vaidya@users.noreply.github.com>
2025-08-14 14:16:46 -0400

    Update main_job.sh 08_14_25

--- main_job.sh
+++ main_job.sh
@@ -1,87 +1,105 @@
 #!/bin/bash
 
-declare -a threads=(1 25 50 100)
+tuples=("(3 15)") #("(2 10)" "(2 16)" "(2 28)" "(3 15)" "(4 16)" "(4 20)" "(5 15)")
 
-declare -a stages=(1 2 5 10)
+# Iterate through each tuple by name
+for t in "${tuples[@]}"; do
+    
+    t="${t//[\(\)]/}"  # Remove parentheses
+    read -r VERTICES TRACKS <<< "$t"
 
-declare -a sweeps=(2 5 10 20)
-
-#declare -a sweeps=(6)
-#DA_SWEEPS=5
-
-mkdir -p slurm
-
-for THREADS in "${threads[@]}"
-do
-    for STAGES in "${stages[@]}"
+    declare -a threads=(1) #25 50 100)
+    
+    declare -a stages=(1) #2 5 10)
+    
+    declare -a sweeps=(27) #5 10 20)
+    
+    #declare -a sweeps=(6)
+    #DA_SWEEPS=5
+    
+    mkdir -p slurm
+    
+    for THREADS in "${threads[@]}"
     do
-        for SWEEPS in "${sweeps[@]}"
+        for STAGES in "${stages[@]}"
         do
-            if (( SWEEPS % STAGES == 0 && SWEEPS != STAGES)); then
-                output_file="ConvergenceEfficiency_and_TimePerAnneal.txt"
-                >"$output_file"
-
-                output_file2="ConvergenceEfficiency_and_DunnIndex_Binning.txt"
-                >"$output_file2"
-            
-                export OMP_PROC_BIND=TRUE       # Enables binding threads to CPUs
-                export OMP_PLACES=cores         # Maps each thread to a distinct core
-                export OMP_NUM_THREADS=${THREADS}  # Set number of threads to your cores count
-                SAMPLES_PER_THREAD=$((10000 / THREADS))
-            
-                echo "Running with THREADS=${THREADS}, STAGES=${STAGES}, SAMPLES_PER_THREAD=${SAMPLES_PER_THREAD}, SWEEPS=${SWEEPS}, DA_SWEEPS=${SWEEPS}"
+            for SWEEPS in "${sweeps[@]}"
+            do
+                if (( SWEEPS % STAGES == 0 && SWEEPS != STAGES)); then
+                    output_file="ConvergenceEfficiency_and_TimePerAnneal.txt"
+                    >"$output_file"
+    
+                    output_file2="ConvergenceEfficiency_and_DunnIndex_Binning.txt"
+                    >"$output_file2"
                 
-                SCRIPT_DIR="job_scripts"
-                mkdir -p "$SCRIPT_DIR"
+                    export OMP_PROC_BIND=TRUE       # Enables binding threads to CPUs
+                    export OMP_PLACES=cores         # Maps each thread to a distinct core
+                    export OMP_NUM_THREADS=${THREADS}  # Set number of threads to your cores count
+                    SAMPLES_PER_THREAD=$((10000 / THREADS))
                 
-                JOB_FILE="${SCRIPT_DIR}/job_${THREADS}_${STAGES}_${SWEEPS}_${SWEEPS}.sh"
-            
-                cat > "$JOB_FILE" << EOF
+                    echo "Running with ${VERTICES} VERTICES and ${TRACKS} TRACKS, THREADS=${THREADS}, STAGES=${STAGES}, SAMPLES_PER_THREAD=${SAMPLES_PER_THREAD}, SWEEPS=${SWEEPS}, DA_SWEEPS=${SWEEPS}"
+                    
+                    SCRIPT_DIR="job_scripts"
+                    mkdir -p "$SCRIPT_DIR"
+                    
+                    JOB_FILE="${SCRIPT_DIR}/job_${VERTICES}_${TRACKS}_${THREADS}_${STAGES}_${SWEEPS}_${SWEEPS}.sh"
+                
+                    cat > "$JOB_FILE" << EOF
 #!/bin/bash
+#SBATCH --account=physics
+#SBATCH --partition=cpu
+#SBATCH --qos=standby
 #SBATCH --job-name=run_${THREADS}
 #SBATCH --nodes=1
 #SBATCH --ntasks=${THREADS}
-#SBATCH --time=01:00:00
+#SBATCH --time=00:10:00
 
 export SAMPLES_PER_THREAD=${SAMPLES_PER_THREAD}
 
 make clean
-make && make run THREADS=${THREADS} STAGES=${STAGES} SWEEPS=${SWEEPS} DA_SWEEPS=${SWEEPS} SAMPLES_PER_THREAD=\$SAMPLES_PER_THREAD
+make && make run VERTICES=${VERTICES} TRACKS=${TRACKS} THREADS=${THREADS} STAGES=${STAGES} SWEEPS=${SWEEPS} DA_SWEEPS=${SWEEPS} SAMPLES_PER_THREAD=${SAMPLES_PER_THREAD}
 EOF
-    
-                chmod +x "$JOB_FILE"
-                
-                # Submit job and capture Job ID
-                jobid=$(sbatch "$JOB_FILE" | awk '{print $4}')
-                echo "Submitted job slurm-$jobid for THREADS=$THREADS , STAGES=$STAGES , SWEEPS=$SWEEPS, DA_SWEEPS=$SWEEPS"
-                
-                echo ""
-                
-                scontrol show job "$jobid"
-                
-                echo ""
-                
-                # Wait for the job to complete before continuing
-                while true; do
-                    state=$(sacct -j "$jobid" --format=State --noheader | awk '{print $1}' | head -n 1)
+        
+                    chmod +x "$JOB_FILE"
                     
-                    squeue -j "$jobid"
-                    #echo "Job slurm-$jobid status: $state"
+                    # Submit job and capture Job ID
+                    jobid=$(sbatch "$JOB_FILE" | awk '{print $4}')
+                    echo "Submitted job slurm-$jobid for VERTICES=$VERTICES, TRACKS=$TRACKS, THREADS=$THREADS , STAGES=$STAGES , SWEEPS=$SWEEPS, DA_SWEEPS=$SWEEPS"
                     
-                    if [[ "$state" == "COMPLETED" || "$state" == "FAILED" || "$state" == "CANCELLED" || "$state" == "TIMEOUT" ]]; then
-                        mv slurm-${jobid}.out slurm/
-                        break
-                    fi
-                
-                    sleep 10
-                done
-                
-                echo ""
-                
-                echo "Job slurm-$jobid ($state) completed. Proceeding to next."
-                
-                echo ""
-            fi
+                    echo ""
+                    
+                    scontrol show job "$jobid"
+                    
+                    echo ""
+                    
+                    # Wait for the job to complete before continuing
+                    while true; do
+                        state=$(sacct -j "$jobid" --format=State --noheader | awk '{print $1}' | head -n 1)
+                        
+                        squeue -j "$jobid"
+                        #echo "Job slurm-$jobid status: $state"
+                        
+                        if [[ "$state" == "COMPLETED" || "$state" == "FAILED" || "$state" == "CANCELLED" || "$state" == "TIMEOUT" ]]; then
+                            mv slurm-${jobid}.out slurm/
+                            break
+                        fi
+                    
+                        sleep 10
+                    done
+                    
+                    echo ""
+                    
+                    echo "Job slurm-$jobid ($state) completed. Proceeding to next."
+                    
+                    echo ""
+                fi
+            done
         done
     done
+    
+    unset threads
+    
+    unset stages
+    
+    unset sweeps
 done
